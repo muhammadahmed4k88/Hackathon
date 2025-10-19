1bb143b8ce9394cd0eb14d2cec409cd9dc1c9920
muhammadahmed <muhammadahmed4k88@gmail.com>
2025-10-18 22:43:25 -0700

    first commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { supabase } from "../../supabaseClient";
+import { supabase } from "../../supabaseclient";
 import  toast  from 'react-hot-toast';
 import { useNavigate } from "react-router-dom";
 
